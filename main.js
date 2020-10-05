c6bd896a4f95a25f66c88dca8107d84bce93a637
Shashank Padwal <shashank23padwal@live.com>
2020-10-05 14:04:46 +0530

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu } = electron;
+let mainWindow;
+app.on("ready", function () {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  const mainMenu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const template = [
+  {
+    label: "Filter",
+    submenu: [
+      {
+        label: "dev-tools",
+        accelerator: "CmdOrCtrl+D",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  },
+];
