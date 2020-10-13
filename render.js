a1ae9bd235627bc5e30d8445161dd7f57b3ce58b
Shashank Padwal <shashank23padwal@live.com>
2020-10-14 00:27:58 +0530

    before dark

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 //imports
-const { writeFile } = require("fs");
+const { writeFile, appendFile } = require("fs");
 const dgram = require("dgram");
 const ip = require("ip");
 const remote = require("electron").remote;
@@ -8,57 +8,66 @@ const { dialog } = remote;
 //dom elements
 var startBtn = document.getElementById("startBtn");
 var closeBtn = document.getElementById("closeBtn");
-var saveBtn = document.getElementById("saveBtn");
 var msgBorad = document.getElementById("msgBorad");
 var dataDiv = document.getElementById("dataDiv");
 
 //defaults
-var dataColor = "red";
 var socket;
-
-//switching colors to disply data changes
-function dataColorToggel() {
-  if (dataColor == "red") dataColor = "blue";
-  else dataColor = "red";
-  return dataColor;
-}
+var data = [];
+const saveLocation = "";
+let saveFilePath = "data.csv";
+let writeMin = 100;
 
 function empty() {
   dataDiv.innerHTML = "";
 }
 
-async function save() {
-  const data = dataDiv.innerText;
-  if (data != "") {
-    var { filePath } = await dialog.showSaveDialog({
-      buttonLabel: "Save Data",
-      defaultPath: `sensorData.csv`,
-    });
-
-    if (filePath) {
-      writeFile(
-        filePath,
-        data,
-        () => (msgBorad.innerHTML = `<span class="green">Data saved!</span>`)
-      );
-    }
-  } else {
-    msgBorad.innerHTML = `<span class="red">No data found, Start listing to get data<span>`;
-  }
-}
 function closeUDP() {
   socket.close();
 }
 
+function showMsg(msg, type = "") {
+  msgBorad.innerHTML = `<span class="${type}">${msg}</span>`;
+  dataDiv.innerHTML += `<p class="${type}">${msg}</p>`;
+  dataDiv.scrollTop = dataDiv.scrollHeight;
+}
+
+async function writeToFile(data) {
+  csvString = "";
+  data.forEach((row) => {
+    csvString += row + "\n";
+  });
+  console.log(csvString);
+  appendFile(saveFilePath, csvString, () =>
+    showMsg(`Writen ${data.length} datapoint to ${saveFilePath}`, "")
+  );
+}
+
 //staring UDP listner
-function startUDP() {
+async function startUDP() {
+  data = [];
+  var { filePath } = await dialog.showSaveDialog({
+    buttonLabel: "Create File to Save Data",
+    defaultPath: `data.csv`,
+  });
+
+  if (filePath) {
+    saveFilePath = filePath;
+    showMsg(`Data will be stored at ${saveFilePath} datapoint to csv`, "blue");
+    writeFile(filePath, "", () => startSocket());
+  }
+}
+function startSocket() {
   socket = dgram.createSocket("udp4", {
     exclusive: false,
   });
-
-  socket.on("message", (msg, req) => {
-    dataDiv.innerHTML += `<span class="${dataColorToggel()}">${msg}</span>`;
-    dataDiv.scrollTop = dataDiv.scrollHeight;
+  socket.on("message", (bytes, req) => {
+    const msg = bytes.toString();
+    data.push(msg);
+    if (data.length >= writeMin) {
+      writeToFile(data);
+      data = [];
+    }
   });
 
   socket.bind("8081");
@@ -66,15 +75,14 @@ function startUDP() {
     const address = socket.address();
     var privateIP = ip.address();
     closeBtn.disabled = false;
-    saveBtn.disabled = true;
     startBtn.disabled = true;
-    msgBorad.innerHTML = `<span class="blue">Listing for UDP packets at ${privateIP}:${address.port}</span>`;
+    showMsg(`Listing for UDP packets at ${privateIP}:${address.port}`, "green");
   });
 
   socket.on("close", () => {
     startBtn.disabled = false;
     closeBtn.disabled = true;
-    saveBtn.disabled = false;
-    msgBorad.innerHTML = `<span class="red">Socket Disconnected</span>`;
+    showMsg(`Stoped`, "red");
+    writeToFile(data);
   });
 }
